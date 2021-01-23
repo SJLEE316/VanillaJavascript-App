1bee25d80e83f18144d64cffdc2d7fcec1522154
SJLEE316 <lsj8249@naver.com>
2021-01-23 17:13:44 +0900

    clock

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 - bg.js
+
   - [Image()](https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/Image)
   - [classList()](https://developer.mozilla.org/ko/docs/Web/API/Element/classList)
+
+- clock.js
+
+  - [Date](https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Date)
---
+++ src/clock.js
@@ -0,0 +1,24 @@
+// Logic
+// Object를 사용해서 현재 시간을 가져옴
+// 1초마다 reset되게 함
+
+const clockContainer = document.querySelector(".clock");
+const clock = document.createElement("h1");
+
+function getTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  clockContainer.appendChild(clock);
+  clock.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
